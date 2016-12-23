54cbf9f8c22a806a3ffd0a7de6633fa87b8dd521
Wany Bae <baepower@me.com>
2016-12-23 16:24:23 +0900

    Create react fundamental repo

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by baepower on 12/21/16.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
